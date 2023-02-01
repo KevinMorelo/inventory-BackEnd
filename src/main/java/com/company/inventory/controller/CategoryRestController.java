48a47e11420626dde75e4510ec529ac80a8bfd4e
Croata <kevinmorelov@hotmail.com>
2023-02-01 14:05:11 -0600

    Ajuste src

--- src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -2,6 +2,8 @@ package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
@@ -69,4 +72,16 @@ public class CategoryRestController {
 		return response;
 	}
 	
+	/**
+	 * Delete categories by id
+	 * Save categories
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> delete(@PathVariable Long id){
+		
+		ResponseEntity<CategoryResponseRest> response = service.deleteById(id);
+		return response;
+	}
 }
